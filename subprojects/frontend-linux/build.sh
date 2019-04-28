ce44007444a6893bcb90d367d75932934305ffbb
Piper McCorkle <piperswe@gmail.com>
2019-04-28 15:01:04 -0500

    Only build on generic kernels

--- subprojects/frontend-linux/build.sh
+++ subprojects/frontend-linux/build.sh
@@ -20,7 +20,7 @@ pushd "$PRIVATE_DIR"/fscompat-linux
 
 echo "obj-m := $OBJECTS" > Kbuild
 ls /lib/modules
-MODULE_DIR="/lib/modules/`ls -1 /lib/modules | head -n1`/build"
+MODULE_DIR="/lib/modules/`ls -1 /lib/modules | grep generic | head -n1`/build"
 if [[ -e "/lib/modules/`uname -r`/build" ]]
 then
     MODULE_DIR="/lib/modules/`uname -r`/build"
