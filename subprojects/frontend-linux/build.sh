1f899eef3ce187c82f5a099c02107d8350b965a4
Piper McCorkle <piperswe@gmail.com>
2019-04-28 14:32:14 -0500

    Generate Kbuild from Meson

---
+++ subprojects/frontend-linux/build.sh
@@ -0,0 +1,24 @@
+set -e
+
+unset CC
+PRIVATE_DIR="$1"
+OUTPUT="$2"
+shift 2
+
+rm -rf "$PRIVATE_DIR"/fscompat-linux
+mkdir -p "$PRIVATE_DIR"
+
+for INPUT in "$@"
+    OBJECTS="$OBJECTS ${`basename "$INPUT"`%.c}.o"
+done
+
+SOURCE_DIR="`dirname "$1"`"
+cp -R "$SOURCE_DIR" "$PRIVATE_DIR"/fscompat-linux
+pushd "$PRIVATE_DIR"/fscompat-linux
+
+echo "obj-m := $OBJECTS" > Kbuild
+make -C /lib/modules/`uname -r`/build M="`pwd`"
+
+popd
+
+mv "$PRIVATE_DIR"/fscompat-linux/fscompat.ko "$OUTPUT"
